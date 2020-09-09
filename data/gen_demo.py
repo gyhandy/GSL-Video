b71f706ede4c57da483f99c7220c6c68bccf4d2d
ReWr1te <tsy199707@gmail.com>
2020-09-09 12:39:23 -0700

    Add all 5 objects and modify moves

--- data/gen_demo.py
+++ data/gen_demo.py
@@ -3,8 +3,28 @@ from blender_for_gsl import *
 # Render video
 render(
 #    path="F:\\MachineLearning\\GSL\\dataset\\test_cube2_bg0_random.avi", 
-    obj_name='cube', 
+    obj_name='torus', 
     color='Toon BSDF', 
-    bg_color='Glossy BSDF', 
+    bg='Glossy BSDF', 
     move=gen_moves()[0]
 )
+
+#def load_textures():
+#    pass
+
+
+#def add_material(obj=None, name=None):
+#    """ Add an existing material to an object. """
+#    if name in D.materials.keys():
+#        obj.data.materials.clear()
+#        obj.data.materials.append(D.materials[name])
+#    else:
+#        print("Wrong material name. ")
+#        
+
+#def add_texture(obj, texture):
+#    if texture in D.textures.keys():
+#        obj.data.textures.clear()
+#        obj.data.textures.append(D.materials[name])
+#    else:
+#        print("Wrong material name. ")
